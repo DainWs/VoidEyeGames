77023241382bf83265c37783ee9a0acafe03d41d
DainWs <jose.ant.du@gmail.com>
2022-04-28 23:34:37 +0200

    Support View

--- void-eye-games-client/src/pages/SupportPage.js
+++ void-eye-games-client/src/pages/SupportPage.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavLink } from 'react-router-dom';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-    </View>
-  );
+class SupportPage extends React.Component {
+  render() {
+    return (
+      <article className='d-flex flex-column m-3'>
+        <section>
+          <h1>Support</h1>
+          <section className='row vh-40' style={{overflow: 'hidden'}}>
+            <div className='col-12 col-sm-6 col-lg-8'>
+              <p className='text-justify m-0 my-2 my-sm-0'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum.
+              </p>
+            </div>
+            <div className='col-12 col-sm-6 col-lg-4'>
+              <div className='w-100 h-100'>
+                <iframe style={{width: '100%', height: '100%'}} scrolling="no" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=es&amp;q=Les%20Rambles,%201%20Barcelona,%20Spain+(Void%20Game%20Eye)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/car-satnav-gps/">Sat Navs</a></iframe>
+              </div>
+            </div>
+          </section>
+          <hr/>
+          <section className='row' style={{maxHeight: '40vh'}}>
+            <div className='col-12'>
+              <p className='text-justify'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </section>
+        </section>
+        <section>
+          <h3>Report</h3>
+          <section className='row' style={{maxHeight: '40vh'}}>
+            <div className='col-12'>
+              <p className='text-justify'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident.
+              </p>
+            </div>
+          </section>
+          <section className='row'>
+            <NavLink className='btn btn-secondary col-12 col-sm-3 m-3 text-primary' to='/report'>Send report</NavLink>
+          </section>
+        </section>
+      </article>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default SupportPage;
